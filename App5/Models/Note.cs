d662e43efabf96c39a846ebd1c82a6cb8f3af738
Omegon226 <69383841+Omegon226@users.noreply.github.com>
2022-12-05 23:48:40 +0300

    gotovo2

---
+++ App5/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App5.Models
+{
+    public class Note
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool HasDrawing { get; set; }
+    }
+}
